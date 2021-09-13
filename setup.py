d11a6fd9b9c0d77fb88d24f68144b65877068b50
Mani <manikumarb125@gmail.com>
2021-09-13 12:19:34 +0530

    Testing Environment Added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import name
 from setuptools import setup, find_packages
 
 setup(
-    name = "DemoMLOPS",
+    name = "src",
     version = "0.0.1",
     description = "Its Sample",
     author = "Mani",
